2d05972310b08758388a5659f7f82df9d93b73d5
art <step.artur87@gmail.com>
2015-11-30 04:50:24 +0200

    Change version to v0.1.1

--- src/main/java/sinlin/Main.java
+++ src/main/java/sinlin/Main.java
@@ -40,7 +40,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * Time: 12:45 AM
  */
 public class Main {
-    public static String version = "sinlin 0.1.0";
+    public static String version = "sinlin v0.1.1";
 
     public static void main(String[] args) {
         long t = System.currentTimeMillis();
