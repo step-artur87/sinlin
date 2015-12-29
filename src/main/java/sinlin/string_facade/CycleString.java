37de410ea70ca2726c0ccc7c40db85c66be28998
art <step.artur87@gmail.com>
2015-12-29 14:32:11 +0200

    Add count and change variable names to a, b, c, d

--- src/main/java/sinlin/string_facade/CycleString.java
+++ src/main/java/sinlin/string_facade/CycleString.java
@@ -11,24 +11,28 @@ import java.util.StringTokenizer;
  */
 public class CycleString extends StringFacadeAbstract
         implements StringFacadeIF {
-    private Double begin = 0.;
-    private Double step = 1.;
-    private Double end = null;
+    private Double a = null;//begin
+    private Double b = null;//step
+    private Double c = null;//end
+    private Double d = null;//count, todo m.b. integer
 
     public CycleString(String string) {
         name = string;
         StringTokenizer stringTokenizer
                 = new StringTokenizer(string, DELIM_CYCLE);
         try {
-            if (stringTokenizer.countTokens() == 3) {
-                begin = Double.parseDouble(stringTokenizer.nextToken());
-                step = Double.parseDouble(stringTokenizer.nextToken());
-                end = Double.parseDouble(stringTokenizer.nextToken());
+            if (stringTokenizer.countTokens() == 4) {
+                a = Double.parseDouble(stringTokenizer.nextToken());
+                b = Double.parseDouble(stringTokenizer.nextToken());
+                c = Double.parseDouble(stringTokenizer.nextToken());
+                d = Double.parseDouble(stringTokenizer.nextToken());
+/*
             } else if (stringTokenizer.countTokens() == 2) {
-                step = Double.parseDouble(stringTokenizer.nextToken());
-                end = Double.parseDouble(stringTokenizer.nextToken());
+                b = Double.parseDouble(stringTokenizer.nextToken());
+                c = Double.parseDouble(stringTokenizer.nextToken());
             } else if (stringTokenizer.countTokens() == 1) {
-                end = Double.parseDouble(stringTokenizer.nextToken());
+                c = Double.parseDouble(stringTokenizer.nextToken());
+*/
             } else {
                 System.out.println("Cycle "
                         + name
@@ -44,18 +48,76 @@ public class CycleString extends StringFacadeAbstract
             );
             System.exit(1);
         }
+
+        if (c == null && d == null) {
+            System.out.println("Cycle "
+                    + name
+                    + " has nor end element nor element count. Exit"
+            );
+            System.exit(1);
+        }
+
+        if (d == 0) {
+            System.out.println("Cycle "
+                    + name
+                    + " has zero count. Exit"
+            );
+            System.exit(1);
+        }
+
+        if (a == null) {
+            if (b != null && c != null && d != null) {
+                a = c - b * d;
+            } else {
+                a = 0.;
+            }
+        }
+
+        if (b == null) {
+            if (c != null && d != null) {
+                b = (c - a) / d;
+            } else if (d != null) {
+                b = 1.;
+
+            } else {
+                b = Math.signum(c - a);
+            }
+        }
+
+        if (b == 0 && d == null) {
+            System.out.println("Cycle "
+                    + name
+                    + " has zero step and has no count. Exit"
+            );
+            System.exit(1);
+        }
+
+        if (d == null) {
+            d = (c - a) / b;
+        } else {
+            d = Math.min(d, (c - a) / b);
+        }
+        if (d.isInfinite() || d.isNaN() || (d <= 0)) {
+            System.out.println("Cycle "
+                    + name
+                    + " count = " +
+                    d +
+                    " . Exit"
+            );
+            System.exit(1);
+        }
     }
 
     @Override
     public int getSize() {
-        return (int) ((end - begin) / step);
+        return (int) ((c - a) / b);
     }
 
     @Override
     public String getValue(Map<String, String> keyMap, int n) {
         String value;
-        double doubleValue = begin + n * step;
-        if (doubleValue > end) {
+        double doubleValue = a + n * b;
+        if (doubleValue > c) {
             System.out.println("Cycle "
                     + this.name
                     + " have not value with number "
