fd98b7ecfdf08e2a3d5cd2a2a458077efd401ebf
art <step.artur87@gmail.com>
2016-02-28 21:41:13 +0200

    create FlatStringTest

---
+++ src/test/java/sinlin/string_facade/FlatStringTest.java
@@ -0,0 +1,22 @@
+package sinlin.string_facade;
+
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: art
+ * Date: 2/28/16
+ * Time: 9:40 PM
+ */
+public class FlatStringTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
