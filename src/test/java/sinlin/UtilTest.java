17b6d2521cb0ee5332b37ac0a22ccfdb72de29aa
art <step.artur87@gmail.com>
2016-02-28 18:13:04 +0200

    Create UtilTest

---
+++ src/test/java/sinlin/UtilTest.java
@@ -0,0 +1,43 @@
+package sinlin;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: art
+ * Date: 2/28/16
+ * Time: 6:12 PM
+ */
+public class UtilTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testMapElementsSizes() throws Exception {
+
+    }
+
+    @Test
+    public void testOneOrEqual() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteForbiddenSymbols() throws Exception {
+
+    }
+
+    @Test
+    public void testPrintErrorInPath() throws Exception {
+
+    }
+}
