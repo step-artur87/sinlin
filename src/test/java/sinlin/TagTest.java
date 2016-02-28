65e8329516de57fb59ddc40514794e1ead2f1973
art <step.artur87@gmail.com>
2016-02-28 18:50:50 +0200

    Create TagTest

---
+++ src/test/java/sinlin/TagTest.java
@@ -0,0 +1,56 @@
+package sinlin;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: art
+ * Date: 2/28/16
+ * Time: 6:45 PM
+ */
+public class TagTest {
+    Tag tag;
+
+    @Before
+    public void setUp() throws Exception {
+        tag = new Tag("tag", null);//todo test null, null
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        tag = null;
+    }
+
+    @Test
+    public void testAttrSizes() throws Exception {
+
+    }
+
+    @Test
+    public void testNodeSizes() throws Exception {
+
+    }
+
+    @Test
+    public void testIsExemplarWritten() throws Exception {
+
+    }
+
+
+    @Test
+    public void testSetText() throws Exception {
+
+    }
+
+    @Test
+    public void testGetNameWithAttr() throws Exception {
+
+    }
+
+    @Test
+    public void testGetText() throws Exception {
+
+    }
+}
