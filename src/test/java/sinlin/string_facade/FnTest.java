2e6d8e08d2cc6f839d63ad1092b09694cb4acbe2
art <step.artur87@gmail.com>
2016-02-29 02:15:34 +0200

    Create FnTest

--- src/test/java/sinlin/string_facade/FnTest.java
+++ src/test/java/sinlin/string_facade/FnTest.java
@@ -1,8 +1,10 @@
 package sinlin.string_facade;
 
+import junit.framework.Assert;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import sinlin.data.odt.OdfData;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,24 +13,38 @@ import org.junit.Test;
  * Time: 9:44 PM
  */
 public class FnTest {
+    OdfData odfData;
+    Fn fn;
+
     @Before
     public void setUp() throws Exception {
-
+        odfData = new OdfData(
+                "src/test/resources/fn_test.ods");
+        Fn.setData(odfData);
+        fn = new Fn("ten");
     }
 
     @After
     public void tearDown() throws Exception {
-
+        odfData = null;
+        fn = null;
     }
 
     @Test
     public void testGetSize() throws Exception {
-
+        Assert.assertEquals(
+                10,
+                fn.getSize());
     }
 
     @Test
     public void testGetValue() throws Exception {
-
+        Assert.assertEquals(
+                "ten1",
+                fn.getValue(null, 0));
+        Assert.assertEquals(
+                "ten10",
+                fn.getValue(null, 9));
     }
 
 }
