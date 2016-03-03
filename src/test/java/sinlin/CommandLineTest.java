c7c27ef705e91f0e4d16fe9a91dff7136dce62ff
art <step.artur87@gmail.com>
2016-03-03 20:17:27 +0200

    Create CommandLineTest (not completed)

---
+++ src/test/java/sinlin/CommandLineTest.java
@@ -0,0 +1,51 @@
+package sinlin;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: art
+ * Date: 3/3/16
+ * Time: 5:04 PM
+ */
+public class CommandLineTest {
+    ArrayList<String[]> args;
+
+    Exporter exporter;
+
+    TagHandler tagHandler;
+
+    @Before
+    public void setUp() throws Exception {
+        tagHandler = new TagHandler();
+        exporter = new Exporter();
+        args = new ArrayList<>();
+        args.add(new String[]{"-g $1;1;10"});
+        args.add(new String[]{"-i /home/art/Documents/sinlin/sinlin/src/test/resources/pathTest.svg"});
+        //fixme args.add(new String[]{"-p", "-d /home/art/Documents/sinlin/sinlin/src/test/resources/fn_test.ods"});
+        //args.add(new String[]{"-V"});
+        //args.add(new String[]{"-h"});
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        tagHandler = null;
+        exporter = null;
+        args = null;
+    }
+
+    @Test
+    public void pathTest() throws Exception {
+        SaxParsing.parse(tagHandler,
+                "src/test/resources/textTest.svg");
+        args.forEach((t) -> {
+            System.out.println("\t" + Arrays.toString(t));
+            Main.main(t);
+        });
+    }
+}
